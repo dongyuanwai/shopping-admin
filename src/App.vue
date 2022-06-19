2285e5c2436e6a63250699c35ad7c281b0f12ca1
dongyuanwai <yuanwaidong@gmail.com>
2022-06-19 15:45:09 +0800

    404页面配置

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <template>
-	<button class=" bg-purple-400">按钮</button>
 	<router-view></router-view>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -4,10 +4,18 @@ import {
 } from 'vue-router'
 
 import Index from '~/pages/index.vue'
-const routes = [{
-    path:"/",
-    component:Index
-}]
+import NotFound from '~/pages/404.vue'
+const routes = [
+    {
+        path:"/",
+        component:Index
+    },
+    { 
+        path: '/:pathMatch(.*)*', 
+        name: 'NotFound', 
+        component: NotFound 
+    },
+]
 const router = createRouter({
     history: createWebHashHistory(),
     routes:routes
