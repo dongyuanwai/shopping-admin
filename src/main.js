a7dbec2e9fb2648750b3766abd0aac5985fbbc4e
dongyuanwai <yuanwaidong@gmail.com>
2022-06-19 15:23:35 +0800

    配置路由

--- src/main.js
+++ src/main.js
@@ -2,11 +2,12 @@ import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
-
 import 'virtual:windi.css'
+import router from './router/index'
 
 
 const app = createApp(App)
 
+app.use(router)
 app.use(ElementPlus)
 app.mount('#app')
