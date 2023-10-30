e07ae44dd554448c25d4bd06cf3ebc6a74e96279
dongyuanwai <yuanwaidong@gmail.com>
2023-10-30 21:16:07 +0800

    feat:路由守卫&权限管理

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import router from './router/index'
 // tailwind css  与 element样式有冲突，如果把 tailwind放到下面 会覆盖掉element某些样式（比如button）
 import './main.css'
 import 'element-plus/dist/index.css'
-
+import "~/permission"
 import { createPinia } from 'pinia'
 
 const app = createApp(App)
--- src/permission.js
+++ src/permission.js
@@ -1,3 +1,22 @@
 // 权限管理控制
-
 import router from '~/router'
+import { getToken } from '~/composables/auth'
+import { toast } from './composables/utils'
+// 全局路由前置守卫
+router.beforeEach((to,from,next)=>{
+    console.log("to",to,from)
+    const token = getToken()
+    // 没有登录就跳转到登录页
+    if(!token && to.path != "/login"){
+        toast("error","请先登录！")
+        next({path:"/login"})
+    }
+
+    // 防止重复登录
+    if(token && to.path == "/login"){
+        toast("error","请勿重复登录！")
+        return next({path:from.path?from.path:'/'})
+    }
+
+    next()
+})
